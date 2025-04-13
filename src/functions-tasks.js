bdf6fcf77f1221bf19fd5eb2b45c253eb4a6012d
Eugeku <eugenekuzora@gmail.com>
2025-04-13 22:45:35 +0300

    feat: getIdGeneratorFunction impl

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -204,7 +204,7 @@ function logger(func, logFunc) {
  *   partialUsingArguments(fn, 'a','b','c','d')() => 'abcd'
  */
 function partialUsingArguments(fn, ...args1) {
-  return function (...args2) {
+  return (...args2) => {
     return fn(...args1, ...args2);
   };
 }
@@ -226,8 +226,13 @@ function partialUsingArguments(fn, ...args1) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let current = startFrom;
+  return () => {
+    const result = current;
+    current += 1;
+    return result;
+  };
 }
 
 module.exports = {
